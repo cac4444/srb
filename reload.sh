6f23644ff8cfd699885190ec94bca1eef31d11a9
cac4444 <127138677+cac4444@users.noreply.github.com>
2025-08-02 16:24:42 +0330

    Create reload.sh

---
+++ reload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "[*] Starting Reloading Script"
+
+# Start service
+echo "[*] Reloading systemd and starting SRBMiner service"
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable srbminer.service
+sudo systemctl restart srbminer.service
+
+# Delete this script after execution
+rm -f "$(realpath "$0")"
